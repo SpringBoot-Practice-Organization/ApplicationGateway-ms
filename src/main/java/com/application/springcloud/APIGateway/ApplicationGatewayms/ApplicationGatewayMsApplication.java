e2929a9b6881638a51652badeaab821c9df92ca5
kalagoudap <43758241+kalagoudap@users.noreply.github.com>
2023-10-07 17:59:26 +0530

    Add files via upload
    Added New Project

---
+++ src/main/java/com/application/springcloud/APIGateway/ApplicationGatewayms/ApplicationGatewayMsApplication.java
@@ -0,0 +1,13 @@
+package com.application.springcloud.APIGateway.ApplicationGatewayms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationGatewayMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationGatewayMsApplication.class, args);
+	}
+
+}
