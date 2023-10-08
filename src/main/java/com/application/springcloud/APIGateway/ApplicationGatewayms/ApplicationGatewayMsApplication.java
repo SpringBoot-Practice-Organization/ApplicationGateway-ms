5f98600f9def0bacd49163229d5b272550f334d9
Nalumeya <godilaxman470@gmail.com>
2023-10-08 22:17:42 +0530

    Added Spring API Gateway and Spring Security

--- src/main/java/com/application/springcloud/APIGateway/ApplicationGatewayms/ApplicationGatewayMsApplication.java
+++ src/main/java/com/application/springcloud/APIGateway/ApplicationGatewayms/ApplicationGatewayMsApplication.java
@@ -2,8 +2,10 @@ package com.application.springcloud.APIGateway.ApplicationGatewayms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApplicationGatewayMsApplication {
 
 	public static void main(String[] args) {
